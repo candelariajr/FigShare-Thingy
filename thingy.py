5f45338efc5b0e7c7fef4011a901f8acd4bdbe7b
Jonathan Candelaria <candelariajr@appstate.edu>
2026-03-31 11:53:45 -0400

    Update thingy.py
    Switch to Sickle as it's a Python library designed to handle OAI-PMH stuff.

--- thingy.py
+++ thingy.py
@@ -1,32 +1,19 @@
-import json
-import reques ts
-from requests.exceptions import HTTPError, RequestException
+from sickle import Sickle
 
-with open("config.json", "r", encoding="utf-8") as f:
-    config = json.load(f)
+s = Sickle("https://api.figshare.com/v2/oai")
 
-apikey = config["api-key"]
-BASE_URL = "https://api.figshare.com/v2"
+count = 0
 
-
-def figshare_get(endpoint):
-    url = f"{BASE_URL}/{endpoint}"
-    headers = {
-        "Authorization": f"token {apikey}"
-    }
-
+for r in s.ListRecords(
+    metadataPrefix="oai_cerif_openaire",
+    set="portal_1148"
+):
     try:
-        response = requests.get(url, headers=headers, timeout=30)
-        response.raise_for_status()
-        return response.json()
-    except HTTPError:
-        print("HTTP error:", response.status_code)
-        print(response.text)
-        raise
-    except RequestException as e:
-        print("Request failed:", e)
-        raise
+        title = r.metadata.get('title', [''])[0]
+        print(title)
+        count += 1
 
-
-articles = figshare_get("account/articles")
-print(articles)
+        if count == 100:
+            break
+    except Exception:
+        continue
