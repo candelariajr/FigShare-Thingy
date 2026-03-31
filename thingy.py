ce5a0f3ff63f29c2b74cd6ecd75450fd777801fb
Jonathan Candelaria <candelariajr@appstate.edu>
2026-03-31 13:24:34 -0400

    Update thingy.py
    Holy CRAP how did I not know Sickle was a thing??!?!?

--- thingy.py
+++ thingy.py
@@ -5,15 +5,28 @@ s = Sickle("https://api.figshare.com/v2/oai")
 count = 0
 
 for r in s.ListRecords(
-    metadataPrefix="oai_cerif_openaire",
+    metadataPrefix="oai_dc",
     set="portal_1148"
 ):
     try:
-        title = r.metadata.get('title', [''])[0]
-        print(title)
-        count += 1
+        md = r.metadata
 
+        title = md.get("title", [""])[0]
+        subjects = md.get("subject", [])
+        resource_type = md.get("type", [])
+        dates = md.get("date", [])
+        descriptions = md.get("description", [])
+
+        print("TITLE:", title)
+        print("SUBJECTS:", subjects)
+        print("TYPE:", resource_type)
+        print("DATES:", dates)
+        print("DESCRIPTION:", descriptions)
+        print("-" * 40)
+
+        count += 1
         if count == 100:
             break
+
     except Exception:
         continue
